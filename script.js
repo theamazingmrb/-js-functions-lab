f68947586a683d1a07b2a922331723fb02510a6d
theamazingmrb <billie.heidelberg.jr@gmail.com>
2024-07-15 17:59:26 -0700

    adding hw files

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('sanity check');
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
